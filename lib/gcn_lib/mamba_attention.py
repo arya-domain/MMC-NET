8a001332aec4845213455a59d9d20c1c4a1c7661
Aryan Das <95759588+arya-domain@users.noreply.github.com>
2025-06-08 01:51:12 +0000

    .

---
+++ lib/gcn_lib/mamba_attention.py
@@ -0,0 +1,186 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from einops import rearrange, repeat
+from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
+try:
+    from causal_conv1d import causal_conv1d_fn, causal_conv1d_update
+except ImportError:
+    causal_conv1d_fn, causal_conv1d_update = None, None
+try:
+    from mamba_ssm.ops.triton.selective_state_update import selective_state_update
+except ImportError:
+    selective_state_update = None
+try:
+    from mamba_ssm.ops.triton.layer_norm import RMSNorm, layer_norm_fn, rms_norm_fn
+except ImportError:
+    RMSNorm, layer_norm_fn, rms_norm_fn = None, None, None
+
+
+class Mamba_Attention_layer(nn.Module):
+    def __init__(
+        self,
+        d_model,
+        d_state=16,
+        d_conv=4,
+        expand=2,
+        dt_rank="auto",
+        dt_min=0.001,
+        dt_max=0.1,
+        dt_init="random",
+        dt_scale=1.0,
+        dt_init_floor=1e-4,
+        conv_bias=True,
+        bias=False,
+        use_fast_path=True,
+        layer_idx=None,
+        device=None,
+        dtype=None,
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.d_model = d_model
+        self.d_state = d_state
+        self.d_conv = d_conv
+        self.expand = expand
+        self.d_inner = int(self.expand * self.d_model)
+        self.dt_rank = math.ceil(self.d_model / 16) if dt_rank == "auto" else dt_rank
+        self.use_fast_path = use_fast_path
+        self.layer_idx = layer_idx
+
+        self.in_proj = nn.Linear(self.d_model, self.d_inner * 2, bias=bias, **factory_kwargs)
+
+        self.conv1d = nn.Conv1d(
+            in_channels=self.d_inner,
+            out_channels=self.d_inner,
+            bias=conv_bias,
+            kernel_size=d_conv,
+            groups=self.d_inner,
+            padding=d_conv - 1,
+            **factory_kwargs,
+        )
+
+        self.activation = "silu"
+        self.act = nn.SiLU()
+
+        self.x_proj = nn.Linear(
+            self.d_inner, self.dt_rank + self.d_state * 2, bias=False, **factory_kwargs
+        )
+        self.dt_proj = nn.Linear(self.dt_rank, self.d_inner, bias=True, **factory_kwargs)
+
+        # Initialize special dt projection to preserve variance at initialization
+        dt_init_std = self.dt_rank**-0.5 * dt_scale
+        if dt_init == "constant":
+            nn.init.constant_(self.dt_proj.weight, dt_init_std)
+        elif dt_init == "random":
+            nn.init.uniform_(self.dt_proj.weight, -dt_init_std, dt_init_std)
+        else:
+            raise NotImplementedError
+
+        # Initialize dt bias so that F.softplus(dt_bias) is between dt_min and dt_max
+        dt = torch.exp(
+            torch.rand(self.d_inner, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min))
+            + math.log(dt_min)
+        ).clamp(min=dt_init_floor)
+        # Inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
+        inv_dt = dt + torch.log(-torch.expm1(-dt))
+        with torch.no_grad():
+            self.dt_proj.bias.copy_(inv_dt)
+        # Our initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
+        self.dt_proj.bias._no_reinit = True
+
+        # S4D real initialization
+        A = repeat(
+            torch.arange(1, self.d_state + 1, dtype=torch.float32, device=device),
+            "n -> d n",
+            d=self.d_inner,
+        ).contiguous()
+        A_log = torch.log(A)  # Keep A_log in fp32
+        self.A_log = nn.Parameter(A_log)
+        self.A_log._no_weight_decay = True
+
+        # D "skip" parameter
+        self.D = nn.Parameter(torch.ones(self.d_inner, device=device))  # Keep in fp32
+        self.D._no_weight_decay = True
+
+        self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
+
+    def forward(self, hidden_states, inference_params=None):
+        """
+        hidden_states: (B, L, D)
+        Returns: same shape as hidden_states
+        """
+        batch, seqlen, dim = hidden_states.shape
+
+        conv_state, ssm_state = None, None
+
+        # We do matmul and transpose BLH -> HBL at the same time
+        xz = rearrange(
+            self.in_proj.weight @ rearrange(hidden_states, "b l d -> d (b l)"),
+            "d (b l) -> b d l",
+            l=seqlen,
+        )
+        if self.in_proj.bias is not None:
+            xz = xz + rearrange(self.in_proj.bias.to(dtype=xz.dtype), "d -> d 1")
+
+        A = -torch.exp(self.A_log.float())  # (d_inner, d_state)
+        # In the backward pass we write dx and dz next to each other to avoid torch.cat
+        if self.use_fast_path and causal_conv1d_fn is not None and inference_params is None:  # Doesn't support outputting the states
+            out = mamba_inner_fn(
+                xz,
+                self.conv1d.weight,
+                self.conv1d.bias,
+                self.x_proj.weight,
+                self.dt_proj.weight,
+                self.out_proj.weight,
+                self.out_proj.bias,
+                A,
+                None,  # input-dependent B
+                None,  # input-dependent C
+                self.D.float(),
+                delta_bias=self.dt_proj.bias.float(),
+                delta_softplus=True,
+            )
+        else:
+            x, z = xz.chunk(2, dim=1)
+            # Compute short convolution
+            if conv_state is not None:
+                conv_state.copy_(F.pad(x, (self.d_conv - x.shape[-1], 0)))  # Update state (B D W)
+            if causal_conv1d_fn is None:
+                x = self.act(self.conv1d(x)[..., :seqlen])
+            else:
+                assert self.activation in ["silu", "swish"]
+                x = causal_conv1d_fn(
+                    x=x,
+                    weight=rearrange(self.conv1d.weight, "d 1 w -> d w"),
+                    bias=self.conv1d.bias,
+                    activation=self.activation,
+                )
+
+            x_dbl = self.x_proj(rearrange(x, "b d l -> (b l) d"))  # (bl d)
+            dt, B, C = torch.split(x_dbl, [self.dt_rank, self.d_state, self.d_state], dim=-1)
+            dt = self.dt_proj.weight @ dt.t()
+            dt = rearrange(dt, "d (b l) -> b d l", l=seqlen)
+            B = rearrange(B, "(b l) dstate -> b dstate l", l=seqlen).contiguous()
+            C = rearrange(C, "(b l) dstate -> b dstate l", l=seqlen).contiguous()
+            assert self.activation in ["silu", "swish"]
+            y = selective_scan_fn(
+                x,
+                dt,
+                A,
+                B,
+                C,
+                self.D.float(),
+                z=z,
+                delta_bias=self.dt_proj.bias.float(),
+                delta_softplus=True,
+                return_last_state=ssm_state is not None,
+            )
+            if ssm_state is not None:
+                y, last_state = y
+                ssm_state.copy_(last_state)
+            y = rearrange(y, "b d l -> b l d")
+            out = self.out_proj(y)
+        return out
---
+++ lib/networks.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from decoders import UGC
+from .maxvit import maxxvit_rmlp_small_rw_256 as maxxvit_rmlp_small_rw_256_4out
+from .maxvit import maxvit_rmlp_small_rw_224 as maxvit_rmlp_small_rw_224_4out
+
+def np2th(weights, conv=False):
+    """Possibly convert HWIO to OIHW."""
+    if conv:
+        weights = weights.transpose([3, 2, 0, 1])
+    return torch.from_numpy(weights)
+
+class LightweightReconstructionModel(nn.Module):
+    def __init__(self):
+        super(LightweightReconstructionModel, self).__init__()
+        self.conv1 = nn.Conv2d(96, 32, kernel_size=3, stride=1, padding=1)
+        self.upsample1 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
+        self.conv_transpose2 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x1, x2 = x
+        x = F.interpolate(x2, size=(x1.shape[-2:]), mode='bilinear')
+        x = x + x1
+        x = F.relu(self.conv1(x))
+        x = self.upsample1(x)
+        x = F.relu(self.conv_transpose2(x))
+        return x
+
+class MMC_NET(nn.Module):
+    def __init__(self, n_class=1, img_size_s1=(256,256), img_size_s2=(224,224), k=11, padding=5, conv='mr', 
+                 gcb_act='gelu', activation='relu', interpolation='bilinear', **kwargs):
+        super(MMC_NET, self).__init__()
+        
+        self.interpolation = interpolation
+        self.img_size_s1 = img_size_s1
+        self.img_size_s2 = img_size_s2
+        self.n_class = n_class
+        
+        # conv block to convert single channel to 3 channels
+        self.conv_1cto3c = nn.Sequential(
+            nn.Conv2d(1, 3, kernel_size=1),
+            nn.BatchNorm2d(3),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.recon = LightweightReconstructionModel()
+        
+        # backbone network initialization with pretrained weight
+        self.backbone1 = maxxvit_rmlp_small_rw_256_4out()  # [64, 128, 320, 512]
+        self.backbone2 = maxvit_rmlp_small_rw_224_4out()  # [64, 128, 320, 512]
+  
+        print('Loading:', './pretrained_pth/maxvit/maxxvit_rmlp_small_rw_256_sw-37e217ff.pth')
+        state_dict1 = torch.load('/media/hdd/alloy2023/Tanmoy/GCASCADE/pretrained_pth/maxvit/maxxvit_rmlp_small_rw_256_sw-37e217ff.pth')        
+        self.backbone1.load_state_dict(state_dict1, strict=False)
+        
+        print('Loading:', './pretrained_pth/maxvit/maxvit_rmlp_small_rw_224_sw-6ef0ae4f.pth')       
+        state_dict2 = torch.load('/media/hdd/alloy2023/Tanmoy/GCASCADE/pretrained_pth/maxvit/maxvit_rmlp_small_rw_224_sw-6ef0ae4f.pth')        
+        self.backbone2.load_state_dict(state_dict2, strict=False)
+        
+        
+        self.channels = [768, 384, 192, 96]
+        
+        # decoder initialization
+        self.decoder1 = UGC(channels=self.channels, img_size=img_size_s1[0], k=k, padding=padding, conv=conv, gcb_act=gcb_act, activation=activation)
+        self.decoder2 = UGC(channels=self.channels, img_size=img_size_s2[0], k=k, padding=padding, conv=conv, gcb_act=gcb_act, activation=activation)
+
+        print('Model %s created, param count: %d' %
+                ('GCASCADE decoder: ', sum([m.numel() for m in self.decoder1.parameters()])))
+        print('Model %s created, param count: %d' %
+                ('GCASCADE decoder: ', sum([m.numel() for m in self.decoder2.parameters()])))
+                
+        # Prediction heads initialization
+        self.out_head1 = nn.Conv2d(self.channels[0], n_class, 1)
+        self.out_head2 = nn.Conv2d(self.channels[1], n_class, 1)
+        self.out_head3 = nn.Conv2d(self.channels[2], n_class, 1)
+        self.out_head4 = nn.Conv2d(self.channels[3], n_class, 1)
+
+        self.out_head4_in = nn.Conv2d(self.channels[3], 1, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        
+        # if grayscale input, convert to 3 channels
+        if x.size()[1] == 1:
+            x = self.conv_1cto3c(x)
+            
+        # transformer backbone as encoder
+        f1 = self.backbone1(F.interpolate(x, size=self.img_size_s1, mode=self.interpolation))
+        recon1 = f1[0]
+        f1 = [f1[0], f1[1], f1[2], f1[3]]
+        
+        # decoder
+        x11_o, x12_o, x13_o, x14_o = self.decoder1(f1[3], [f1[2], f1[1], f1[0]])
+
+        # prediction heads  
+        p11 = self.out_head1(x11_o)
+        p12 = self.out_head2(x12_o)
+        p13 = self.out_head3(x13_o)
+        p14 = self.out_head4(x14_o)
+
+        p14_in = self.out_head4_in(x14_o)
+        p14_in = self.sigmoid(p14_in)
+        
+
+        p11 = F.interpolate(p11, scale_factor=32, mode=self.interpolation)
+        p12 = F.interpolate(p12, scale_factor=16, mode=self.interpolation)
+        p13 = F.interpolate(p13, scale_factor=8, mode=self.interpolation)
+        p14 = F.interpolate(p14, scale_factor=4, mode=self.interpolation)
+
+        
+        p14_in = F.interpolate(p14_in, scale_factor=4, mode=self.interpolation)        
+        p14_in = F.interpolate(p14_in, size=(x.size(2), x.size(3)), mode=self.interpolation)
+        x_in = x * p14_in
+                
+        f2 = self.backbone2(F.interpolate(x_in, size=self.img_size_s2, mode=self.interpolation))
+        recon2 = f2[0]
+        f2 = [f2[0], f2[1], f2[2], f2[3]]
+                    
+        skip1_0 = F.interpolate(f1[0], size=(f2[0].shape[-2:]), mode=self.interpolation)
+        skip1_1 = F.interpolate(f1[1], size=(f2[1].shape[-2:]), mode=self.interpolation)
+        skip1_2 = F.interpolate(f1[2], size=(f2[2].shape[-2:]), mode=self.interpolation)
+        skip1_3 = F.interpolate(f1[3], size=(f2[3].shape[-2:]), mode=self.interpolation)
+
+        
+        x21_o, x22_o, x23_o, x24_o = self.decoder2(f2[3]+skip1_3, [f2[2]+skip1_2, f2[1]+skip1_1, f2[0]+skip1_0])
+
+        p21 = self.out_head1(x21_o)
+        p22 = self.out_head2(x22_o)
+        p23 = self.out_head3(x23_o)
+        p24 = self.out_head4(x24_o)
+
+        p21 = F.interpolate(p21, size=(p11.shape[-2:]), mode=self.interpolation)
+        p22 = F.interpolate(p22, size=(p12.shape[-2:]), mode=self.interpolation)
+        p23 = F.interpolate(p23, size=(p13.shape[-2:]), mode=self.interpolation)
+        p24 = F.interpolate(p24, size=(p14.shape[-2:]), mode=self.interpolation)
+        
+        p1 = p11 + p21
+        p2 = p12 + p22
+        p3 = p13 + p23
+        p4 = p14 + p24
+        
+        recon_out = self.recon([recon1, recon2]) 
+        
+        
+        return [p1, p2, p3, p4] , recon_out
+
+if __name__ == '__main__':
+    model = MERIT_GCASCADE()
+    input_tensor = torch.randn(1, 3, 224, 224)
+
+    p1, p2, p3, p4 = model(input_tensor)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total parameters in the model: {total_params}")
+    print(p1.size(), p2.size(), p3.size(), p4.size())
